88c6b26cdab91dff9fa95ab738444c56509667a8
Planet-X <planetx.dev@gmail.com>
2018-01-02 22:04:49 +0100

    BoardConfig.mk: Update brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,8 +67,7 @@ TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 
-# original path was /sys/devices/soc/1a00000.qcom,mdss_mdp/1a00000.qcom,mdss_mdp:qcom,mdss_fb_primary/leds/lcd-backlight/brightness
-TW_BRIGHTNESS_PATH := "/sys/devices/soc/1a00000.qcom\x2mdss_mdp/1a00000.qcom\x2mdss_mdp:qcom\x2mdss_fb_primary/leds/lcd-backlight/brightness"
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 255
 
 TARGET_RECOVERY_DEVICE_MODULES := \
