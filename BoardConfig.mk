Planet-X/android_device_asus_ze552kl

Move kernel image to prebuilt

BoardConfig.mk
@@ -32,7 +32,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := ze552kl-userdebug_defconfig
-TARGET_PREBUILT_KERNEL := device/asus/Z012/kernel
+TARGET_PREBUILT_KERNEL := device/asus/Z012/prebuilt/kernel
 
 TARGET_LDPRELOAD := libNimsWrap.so
 

Planet-X/android_device_asus_zenfone3

BoardConfig.mk
@@ -32,7 +32,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := ze520kl-userdebug_defconfig
-TARGET_PREBUILT_KERNEL := device/asus/Z017/kernel
+TARGET_PREBUILT_KERNEL := device/asus/Z017/prebuilt/kernel
 
 TARGET_LDPRELOAD := libNimsWrap.so
 